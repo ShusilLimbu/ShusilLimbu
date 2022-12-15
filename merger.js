a3c76e45f835737316e32e3055de948821c1c5cd
shusillimbu <shusillimbupangdhak@gmail.com>
2022-12-15 12:05:30 +0545

    merger of the PDF merger v-1.0

--- merger.js
+++ merger.js
@@ -1,12 +1,12 @@
-const PDFmerger=require('pdf-merger-js');
-const merger=new PDFmerger();
+const PDFmerger = require('pdf-merger-js');
+const merger = new PDFmerger();
 
-     let Mer=async(p1,p2)=>{
-        await merger.add(p1);
-        await merger.add(p2);
-        let d=new Date().getTime();
-        await merger.save(`public/${d}.pdf`);
-        return d;
-
-    };
-    module.exports=Mer;
+let Mer = async (p1,p2,s1,s2) => {
+    console.log(s1,s2);
+    await merger.add(p1);
+    await merger.add(p2,[s1,s2]);
+    let d = new Date().getTime();
+    await merger.save(`public/${d}.pdf`);
+    return d;
+};
+module.exports=Mer;
