a030fe5131a25f8513c0433dd39477f8a012774b
shusillimbu <shusillimbupangdhak@gmail.com>
2022-12-15 10:09:34 +0545

    Server of PDF Merger

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const express=require('express');
+const app=new express();
+const path=require('path');
+const multer=require('multer');
+const upload=multer({dest:'uploads/'});
+const Mer=require('./mer.js');
+
+ 
+const port=process.env.PORT|| 5656;
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.use('/static',express.static('public'));
+
+app.get('/',(req,res)=>{
+
+
+    res.render('dashboard');
+});
+
+app.post('/merger',upload.array('pdfs',2),async(req,res)=>{
+    console.log(req.files[0].path);
+    console.log(req.files[1].path);
+    let d=await Mer(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path));
+    res.redirect(`http://localhost:5656/static/${d}.pdf`);
+
+}); 
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+});
