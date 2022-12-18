434aa85ffec63ab31d6f0830ce24505d51fb2b97
shusillimbu <shusillimbupangdhak@gmail.com>
2022-12-18 10:11:59 +0545

    My PDF Merger Project

--- merger.js
+++ merger.js
@@ -1,12 +1,45 @@
-const PDFmerger = require('pdf-merger-js');
-const merger = new PDFmerger();
+const PDFMerger = require('pdf-merger-js');
+const merger = new PDFMerger();
 
-let Mer = async (p1,p2,s1,s2) => {
-    console.log(s1,s2);
+//if Fnum1 and Snum2 are available
+let Mer = async (p1, p2, f1, f2, s1, s2) => {
+    await merger.add(p1, [f1, f2]);
+    await merger.add(p2, [s1, s2]);
+    let d = new Date().getTime();
+    await merger.save(`public/${d}.pdf`);
+    return d;
+};
+//if Snum1==''
+let Mer2 = async (p1, p2, f1, f2) => {
+    await merger.add(p1, [f1, f2]);
+    await merger.add(p2);
+    let d = new Date().getTime();
+    await merger.save(`public/${d}.pdf`);
+    return d;
+};
+//if Fnum1==''
+let Mer3 = async (p1,p2,s1,s2)=>{
+    console.log(s1, s2);
     await merger.add(p1);
     await merger.add(p2,[s1,s2]);
     let d = new Date().getTime();
     await merger.save(`public/${d}.pdf`);
     return d;
 };
-module.exports=Mer;
+//if Fnum1 and Snum1 ==''
+let Mer4 = async (p1, p2) => {
+    await merger.add(p1);
+    await merger.add(p2);
+    let d = new Date().getTime();
+    await merger.save(`public/${d}.pdf`);
+    return d;
+};
+
+
+
+module.exports = { Mer, Mer2, Mer3, Mer4 };
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,35 +1,62 @@
 
-const express=require('express');
-const app=new express();
-const path=require('path');
-const multer=require('multer');
-const upload=multer({dest:'uploads/'});
-const Mer=require('./merger.js');
+const express = require('express');
+const app = new express();
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const {Mer,Mer2,Mer3, Mer4} = require('./merger.js');
 
- 
-const port=process.env.PORT|| 5656;
 
-app.use(express.static(path.join(__dirname,'public')));
+const port = process.env.PORT || 5656;
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine','ejs');
-app.use('/static',express.static('public'));
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
 
-app.get('/',(req,res)=>{
-
-
-    res.render('dashboard');
+app.get('/', (req, res) => {
+    res.render('dashboard',{error:'Welcome to PDF Merger'});
 });
 
-app.post('/merger',upload.array('pdfs',2),async(req,res)=>{
-    console.log(req.files[0].path);
-    console.log(req.files[1].path);
-    let d=await Mer(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path),req.body.num1,req.body.num2);
-    res.redirect(`http://localhost:5656/static/${d}.pdf`);
+app.post('/merger', upload.array('pdfs', 2), async (req, res) => {
+    console.log(typeof(req.body.Fnum1));
+    console.log("Fnum1"+req.body.Fnum1);
+    console.log("Fnum2"+req.body.Fnum2);
+    console.log("Snum1"+req.body.Snum1);
+    console.log("Snum1"+req.body.Snum2);
+    console.log(Number.isInteger(parseInt(req.body.Fnum1)));
 
-}); 
+    if(Number.isInteger(Number.parseInt(req.body.Fnum2))&&Number.isInteger(Number.parseInt(req.body.Snum2))){
+    
+        let d = await Mer(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path), req.body.Fnum1, req.body.Fnum2, req.body.Snum1, req.body.Snum2);
+        res.redirect(`http://localhost:5656/static/${d}.pdf`);
+    }
+    
+    else if(req.body.Fnum1=='' && req.body.Snum1==''){
+        let d=await Mer4(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path));
+        res.redirect(`http://localhost:5656/static/${d}.pdf`);
+    }
+    else if(req.body.Snum1=='' && Number.isInteger(parseInt(req.body.Fnum1))){
+        let d = await Mer2(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path), req.body.Fnum1, req.body.Fnum2);
+        res.redirect(`http://localhost:5656/static/${d}.pdf`);
+    }
+    else if(req.body.Fnum1=='' && Number.isInteger(parseInt(req.body.Snum1))){
+        let d = await Mer3(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path), req.body.Snum1, req.body.Snum2);
+        res.redirect(`http://localhost:5656/static/${d}.pdf`);
+    }
 
-app.listen(port,()=>{
+    else{
+        res.render('dashboard',{error:"** Error while selecting !!"});
+    }
+    
+    
+    // let d = await Mer(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path), req.body.Fnum1, req.body.Fnum2, req.body.Snum1, req.body.Snum2);
+    // res.redirect(`http://localhost:5656/static/${d}.pdf`);
+
+});
+
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
